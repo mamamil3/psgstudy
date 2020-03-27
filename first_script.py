48fd0d31f3b2dee821b267370a2f05a3b9d8684c
mamamil3 <62665573+mamamil3@users.noreply.github.com>
2020-03-28 03:11:34 +0900

    Create first_script.py

---
+++ first_script.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python3
+print("Output #1: I'm excited to learn Python.")
+
+#두 수를 더한다.
+x = 4
+y = 5
+z = x + y
+print("Output #2: Four plus five equals {0:d}.".format(z))
+
+#두 리스트를 더한다.
+a = [1,2,3,4]
+b = ["first", "second", "third", "fourth"]
+c = a + b
+print("Output #3: {0}, {1}, {2}".format(a,b,c))
