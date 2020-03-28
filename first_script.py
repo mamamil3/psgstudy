1edad30098399a7887f110fe319e7db9a21a3d24
mamamil3 <62665573+mamamil3@users.noreply.github.com>
2020-03-29 04:53:36 +0900

    텍스트파일 읽기 추가

--- first_script.py
+++ first_script.py
@@ -1,4 +1,7 @@
 #! /usr/bin/env python3
+from operator import itemgetter
+import sys
+
 print("Output #1: I'm excited to learn Python.")
 
 #두 수를 더한다.
@@ -12,3 +15,20 @@ a = [1,2,3,4]
 b = ["first", "second", "third", "fourth"]
 c = a + b
 print("Output #3: {0}, {1}, {2}".format(a,b,c))
+
+#파일 읽기
+#하나의 텍스트 파일 읽기(과거방식)
+print("Output #139: ")
+input_file = sys.argv[1]
+filereader = open(input_file, 'r')
+for row in filereader:
+    print(row.strip())
+filereader.close()
+
+#파일 읽기 새방식
+input_file = sys.argv[1]
+print("Output #140: ")
+with open(input_file, 'r', newline='') as filereader:
+    for row in filereader:
+        print("{}".format(row.strip()))
+
