a44cb1ffebff60531a2251e715b036c6b82152f7
mamamil3 <62665573+mamamil3@users.noreply.github.com>
2020-04-04 02:48:43 +0900

    파일읽고 쓰기 연습

--- 2excel_parsing_and_write.py
+++ 2excel_parsing_and_write.py
@@ -4,4 +4,19 @@ Created on Mon Mar 30 02:55:11 2020
 
 @author: yomer
 """
+import sys
+from xlrd import open_workbook
+from xlwt import Workbook
 
+input_file = sys.argv[1]
+output_file = sys.argv[2]
+
+output_workbook = Workbook()
+output_worksheet = output_workbook.add_sheet('Sheet1_output')
+
+with open_workbook(input_file) as workbook:
+    worksheet = workbook.sheet_by_name('Sheet1')
+    for row_index in range(worksheet.nrows):
+        for column_index in range(worksheet.ncols):
+            output_worksheet.write(row_index, column_index, worksheet.cell_value(row_index, column_index))
+output_workbook.save(output_file)
