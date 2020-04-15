df6e07ece4803505916a93c4b40f1c027e055b81
mamamil3 <62665573+mamamil3@users.noreply.github.com>
2020-04-16 00:25:41 +0900

    20200416

--- naver_shopping_search.py
+++ naver_shopping_search.py
@@ -14,13 +14,10 @@ from bs4 import BeautifulSoup
 txtpath = sys.argv[1]
 shopname = sys.argv[2]
 
-#엑셀 파일에 기록하기
-def makeexel(items,path):
-    result=[]
-    for item in items:
-        result.append([item.title.get_text()]+[item.mallname.get_text()])
-    table = pd.DataFrame(result,columns=('title','mallname'))
-    table.to_csv(path,encoding="cp949",mode="w",index=True)
+
+
+
+
     
 # 텍스트파일에서 키 아이디, 시크릿 읽어오기
 keyfile = open(txtpath,'r')
@@ -39,8 +36,10 @@ client_secret = idsecret[1]
 display = 100
 start = 1
 keyword = input("검색어 입력:")
+result=[]
+path = "C:/Users/yomer/Documents/파이썬스터디/"+keyword+"_keywordcount.csv"
 
-def searchwb(display,start,keyword):
+def searchwb(display,start,keyword,page_count):
     url = "https://openapi.naver.com/v1/search/shop.xml"
     option = "&display="+str(display)+"&start="+str(start)    
     query = "?query="+urllib.parse.quote(keyword)
@@ -53,7 +52,7 @@ def searchwb(display,start,keyword):
     rescode = response.getcode()
     if(rescode==200):
         response_body = response.read()
-        #print(response_body.decode('utf-8'))
+        
         
         xmlsoup = BeautifulSoup(response_body,'html.parser')
         items = xmlsoup.find_all('item')
@@ -64,20 +63,28 @@ def searchwb(display,start,keyword):
                 print('<--------------------------')
                 print('제목 : ' + item.title.get_text())
                 print('샵명 : ' + item.mallname.get_text())
-                print('순위 : ' + str(itemcount))
+                print('순위 : ' + str(page_count+1) + 'page ' + str(itemcount-(display*page_count)))
                 print('-------------------------->\n')
+                
+                #엑셀파일에 쓰기 위한 값 리스트 추가
+                result.append([item.title.get_text()]+[item.mallname.get_text()]+[str(itemcount-(display*page_count))]+[page_count+1])
                 #sys.exit()
             itemcount += 1
-        #makeexel(items, "C:/Users/yomer/Documents/파이썬스터디/navershoping_keywordcount.csv")
     else:
         print("Error Code:" + rescode)
     
+#api 검색 최대치 1000건 이므로 1회한도 100건씩 10번 읽어오기!    
+for i in range(0,25):
     
-for i in range(0,10):
     print(start)
-    searchwb(display,start,keyword)
-    if start < 1000:
-        start = start + display
+    #검색어로 상품찾기 함수 호출
+    searchwb(display,start,keyword,i)
     
+    if start < 500:
+        start = start + display
+
+#엑셀 파일에 기록하기
+table = pd.DataFrame(result,columns=('title','mallname','ranking','page_count'))
+table.to_csv(path,encoding="cp949",mode="w",index=True)    
 
 
