bdfe7857c0dc0c62d1b919a068137a616124751c
mamamil3 <62665573+mamamil3@users.noreply.github.com>
2020-06-04 21:48:51 +0900

    Update pandas_value_meets_condition_all_worksheets.py

--- pandas_value_meets_condition_all_worksheets.py
+++ pandas_value_meets_condition_all_worksheets.py
@@ -10,8 +10,8 @@ import sys
 input_file = sys.argv[1]
 output_file = sys.argv[2]
 
-# 모든 시트 읽기 sheetname=None
-data_frame = pd.read_excel(input_file, sheetname = None, index_col=None)
+# 모든 시트 읽기 sheet_name=None
+data_frame = pd.read_excel(input_file, sheet_name = None, index_col=None)
 
 row_output = []
 
