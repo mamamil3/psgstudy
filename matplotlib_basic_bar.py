3ef0f58e8c08a3d461230c75bef41da7c19a8bb3
mamamil3 <62665573+mamamil3@users.noreply.github.com>
2020-06-13 22:47:56 +0900

    20200613

--- matplotlib_basic_bar.py
+++ matplotlib_basic_bar.py
@@ -12,12 +12,18 @@ customers_index = range(len(customers))
 sale_amounts = [127, 90, 201, 111, 232]
 fig = plt.figure()
 ax1 = fig.add_subplot(1,1,1)
-ax1.bar(customers_index, sale_amounts, align='center', color='darkblue')
+rects = ax1.bar(customers_index, sale_amounts, align='center', color='darkblue')
 ax1.xaxis.set_ticks_position('bottom')
 ax1.yaxis.set_ticks_position('left')
+
+# 막대그래프에 y값의 label표시하기
+for p in ax1.patches:
+    left, bottom, width, height = p.get_bbox().bounds
+    ax1.annotate(height, (left+width/2, height*1.01), ha='center')
+
 plt.xticks(customers_index, customers, rotation=0, fontsize='small')
 plt.xlabel('Customer Name')
 plt.ylabel('Sale Amount')
 plt.title('Sale Amount per Customer')
-plt.savefig('bar_plot.png', dpi=400, bbox_inches='tight')
+#plt.savefig('bar_plot.png', dpi=400, bbox_inches='tight')
 plt.show()
