50faf5bf03ed1e3cf2f62b779f0f123d56314fd4
mamamil3 <62665573+mamamil3@users.noreply.github.com>
2020-07-05 23:10:50 +0900

    0705

--- convert_ss_invoicelist_template.py
+++ convert_ss_invoicelist_template.py
@@ -18,13 +18,13 @@ output_file = 'ssInvoiceList_' + dt.strftime("%Y%m%d") + '.xls'
 print('input_file: ' + input_file)
 
 # 첫번째 시트 선택, 첫행 해더 선택 디폴트값
-input_data_frame = pd.read_excel(input_file, sheet_name=0, header=0, dtype={'솔루션 주문번호':str }, index_col=None)
+input_data_frame = pd.read_excel(input_file, sheet_name=0, header=0, dtype={'솔루션 주문번호':str, '송장번호':str }, index_col=None)
 
 data_frame_column_by_name = input_data_frame.loc[:, ['솔루션 주문번호', '배송구분', '택배사', '송장번호']]
 data_frame_column_by_name.columns = ['상품주문번호', '배송방법', '택배사', '송장번호']
 data_frame_column_by_name['배송방법'] = '택배,등기,소포'
 
 writer = pd.ExcelWriter(output_file)
-data_frame_column_by_name.to_excel(writer, sheet_name='Sheet1', index=False)
+data_frame_column_by_name.to_excel(writer, sheet_name='발송처리', index=False)
 writer.save()
 
