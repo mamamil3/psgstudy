66b4e3e095f752222a50e4f62f993f93c4f0a316
mamamil3 <62665573+mamamil3@users.noreply.github.com>
2020-07-08 22:27:06 +0900

    Update fv_function.py

--- fv_function.py
+++ fv_function.py
@@ -8,3 +8,9 @@ Created on Mon Jul  6 23:20:46 2020
 
 def fv_f(pv,r,n):
     return pv*(1+r)**n
+
+def npv_f(rate, cashflows):
+    total = 0.0
+    for i in range(0,len(cashflows)):
+        total += cashflows[i] / (1 + rate)**i
+    return total
