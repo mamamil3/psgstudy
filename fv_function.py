caff9798f1a560ec052627619c2219e2e304b3b7
mamamil3 <62665573+mamamil3@users.noreply.github.com>
2020-07-15 23:42:59 +0900

    0715

--- fv_function.py
+++ fv_function.py
@@ -5,10 +5,10 @@ Created on Mon Jul  6 23:20:46 2020
 @author: yomer
 """
 
-"""
+
 def fv_f(pv,r,n):
     return pv*(1+r)**n
-"""
+
 
 def npv_f(rate, cashflows):
     total = 0.0
@@ -16,13 +16,13 @@ def npv_f(rate, cashflows):
         total += cashflows[i] / (1 + rate)**i
     return total
 
-"""
+
 def npv_fe(rate, cashflows):
     total = 0.0
     for i, cashflow in enumerate(cashflows):
         total += cashflow / (1 + rate)**i
     return total
-"""
+
 
 #npv를 0으로 만드는 할인율 계산식
 cashFlows=(550,-500,-500,-500,1000)
