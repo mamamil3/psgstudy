3b1fcefc0a76e487478290981a26383bd84d0f12
mamamil3 <62665573+mamamil3@users.noreply.github.com>
2020-07-22 22:57:34 +0900

    0722

--- readbypandas.py
+++ readbypandas.py
@@ -6,6 +6,7 @@ Created on Mon Jul 20 23:29:21 2020
 """
 import pandas as pd
 
-url='httP://canisius.edu/~yany/data/ibm.csv'
-x=pd.read_csv(url)
+#url='httP://canisius.edu/~yany/data/ibm.csv'
+path = 'test.csv'
+x=pd.read_csv(path)
 print(x[1:5])
