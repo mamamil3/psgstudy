afff34a4f451a0a1e0a2635bf937c993c86c4b61
mamamil3 <62665573+mamamil3@users.noreply.github.com>
2020-07-23 23:46:17 +0900

    0723

--- readbypandas.py
+++ readbypandas.py
@@ -4,9 +4,17 @@ Created on Mon Jul 20 23:29:21 2020
 
 @author: yomer
 """
-import pandas as pd
+# import pandas as pd
 
-#url='httP://canisius.edu/~yany/data/ibm.csv'
-path = 'test.csv'
-x=pd.read_csv(path)
-print(x[1:5])
+# #url='httP://canisius.edu/~yany/data/ibm.csv'
+# path = 'test.csv'
+# x=pd.read_csv(path)
+# print(x[1:5])
+import pandas_datareader.data as web
+import datetime
+start = datetime.datetime(2016, 2, 19)
+end = datetime.datetime(2016, 3, 4)
+
+df = web.DataReader("078930.KS", "yahoo", start, end)
+
+print(df)
