006bce35cec1d90b6d742657e72fa0f03ce61e2d
mamamil3 <62665573+mamamil3@users.noreply.github.com>
2020-07-27 23:50:28 +0900

    0727

--- readbypandas.py
+++ readbypandas.py
@@ -12,9 +12,11 @@ Created on Mon Jul 20 23:29:21 2020
 # print(x[1:5])
 import pandas_datareader.data as web
 import datetime
+
 start = datetime.datetime(2020, 2, 19)
 end = datetime.datetime(2020, 3, 4)
 
-df = web.DataReader("078930.KS", "yahoo", start, end)
+# df = web.DataReader("078930.KS", "yahoo", start, end)
+df =web.get_data_yahoo("078930.KS")
 
 print(df)
